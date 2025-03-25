0ff3c5e4d3d569fbc40a353322ca0291769247e1
Muhammad Hammad Faisal <hammadfaisal178@gmail.com>
2025-03-25 12:01:47 +0500

    feat: custom filter added

---
+++ src/apps/users/templatetags/custom_filters.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='snake_to_title')
+def snake_to_title(value):
+    """Convert snake_case to Title Case."""
+    if not isinstance(value, str):
+        return value
+    return value.replace("_", " ").title()
