77a0fa4d67e26d3aedd883a98b613d0e34454e15
Muhammad Hammad Faisal <hammadfaisal178@gmail.com>
2025-03-25 11:41:40 +0500

    feat: task_manager project created

---
+++ task_manager/settings.py
@@ -0,0 +1,115 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "3-0g@h@#601(y*ff_a8%knwnaud@bx+zc03q7yqry1l#tnh1%_")
+
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+
+# Application definition
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "channels",
+    "src.apps.users",
+    "src.apps.tasks",
+    "src.apps.notifications"
+]
+
+ASGI_APPLICATION = "task_manager.asgi.application"
+
+AUTH_USER_MODEL = "users.CustomUser"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [os.environ.get("REDIS_URL", "redis://localhost:6379")],
+        },
+    },
+}
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "task_manager.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "src/templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
---
+++ task_manager/urls.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.urls import path
+from src.apps.users import views as user_views
+from src.apps.tasks import views as task_views
+from src.apps.notifications import views as notification_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+                  path("", task_views.home, name="home"),
+                  path("admin/", admin.site.urls),
+                  path("signup/", user_views.signup, name="signup"),
+                  path("login/", user_views.login_view, name="login"),
+                  path("logout/", user_views.logout_view, name="logout"),
+                  path("dashboard/", task_views.dashboard, name="dashboard"),
+                  path("tasks/", task_views.task_list, name="task_list"),
+                  path("tasks/<int:task_id>/", task_views.task_detail, name="task_detail"),
+                  path("tasks/<int:task_id>/edit/", task_views.task_edit, name="task_edit"),
+                  path("tasks/<int:task_id>/delete/", task_views.task_delete, name="task_delete"),
+                  path("tasks/create/", task_views.task_create, name="task_create"),
+                  path("teams/", user_views.team_list, name="team_list"),
+                  path("teams/create/", user_views.team_create, name="team_create"),
+                  path("teams/join/", user_views.team_join, name="team_join"),
+                  path("notifications/", notification_views.notification_list, name="notification_list"),
+                  path("notifications/<int:notification_id>/read", notification_views.notification_read, name="notification_read"),
+                  path("profile/", user_views.profile, name="profile"),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
